664e58b701e5b6f60bbbf4a27e80937f3079b3b2
Francisco Gonzalez <negrocu@MacBook-Pro-de-Francisco.local>
2018-06-20 02:12:43 -0400

    Fix

--- lib/background_service.rb
+++ lib/background_service.rb
@@ -1,11 +1,14 @@
 require 'rubygems'
 require 'mqtt'
+require 'yaml'
 require_relative 'data_manage.rb'
 require_relative '../config/databse.rb'
 
 ARGV.each do |arg|
     @server = arg
 end
+
+config = YAML.load_file(File.join(File.dirname(__FILE__),'../config/ttn_conf.yml'))
 
 @manage = DataManage.new()
 puts "Connecting"
@@ -15,3 +18,4 @@ puts "MQTT Client up"
 @client.get('backend/#') do |topic,message|
     @manage.check(topic,message)
 end
+
