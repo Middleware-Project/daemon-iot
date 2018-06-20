ae1e750ef201be6fcbe25c73fce735d52fa863ac
Francisco Gonzalez <negrocu@MacBook-Pro-de-Francisco.local>
2018-06-20 02:13:11 -0400

    Se agrega soporte ttn

--- lib/data_manage.rb
+++ lib/data_manage.rb
@@ -26,6 +26,27 @@ class DataManage
         end
     end
 
+    def measure_ttn_manage(topic,message)
+        message = JSON.parse(message)
+        puts message['payload_fields']['data']
+        # @node = Node.find_by_id(topic[2])
+        # @group = Group.find_by_id(topic[1])
+        # if @node != nil && @group != nil
+        #     check = @node.sensors.find_by_name(topic[5])
+        #     if check != nil
+        #         value = message[topic[5]]['value']
+        #         unique_id = message[topic[5]]['unique_id']
+        #         unique_id = /([a-z0-9])\w+/.match(unique_id)
+        #         Measure.create(:data => value, :unit => check.units,:node => @node,:sensor => check)
+        #         puts "Datos ingresados"
+        #     else
+        #         puts "Sensor de #{topic[5]} no pertenece a nodo #{@node.id}"
+        #     end
+        # else
+        #     puts "Datos incorrectos"
+        # end
+    end
+
     def heartbeat_manage(topic, message)
         @node = Node.find_by_id(topic[2])
         if @node != nil
@@ -55,6 +76,8 @@ class DataManage
             if topic[4] == 'heartbeat'
                 heartbeat_manage(topic,message)
             end
+        elsif topic[1] == 'ttn'
+            measure_ttn_manage(topic,message)
         else
             puts "Topico incorrecto"
         end
